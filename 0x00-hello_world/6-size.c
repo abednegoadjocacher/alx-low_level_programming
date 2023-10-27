653d7a93fdddb0a6eb6decb34a535387d58f1e81
Adjocacher Abednego <abednegoadjocacher@gmail.com>
2023-10-26 19:57:28 -0700

    Checking the size data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,15 +5,10 @@
  */
 int main(void)
 {
-char c;
-int n;
-long int b;
-long  long int a;
-float f;
-	printf("Size of a 'char': %lu byte(s)\n", sizeof(c));
-	pirntf("Size of an 'int': %lu byte(s)\n", sizeof(n));
-	pirntf("Size of a 'long int': %lu byte(s)\n", sizeof(b));
-	pirntf("Size of a long long 'int': %lu byte(s)\n", sizeof(a));
-	pirntf("Size of a 'float': %lu byte(s)\n", sizeof(f));
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	pirntf("Size of an int: %d byte(s)\n", sizeof(int));
+	pirntf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	pirntf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	pirntf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
