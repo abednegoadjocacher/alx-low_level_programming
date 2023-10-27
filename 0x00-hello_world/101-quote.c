a3009044bc061ca337bf7bd18e604ebab8f6cb44
Adjocacher Abednego <abednegoadjocacher@gmail.com>
2023-10-26 21:08:33 -0700

    Printing using write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,12 @@
-#include <unstdio.h>
+#include <stdio.h>
+#include <unistd.h>
 /**
  * main - Not using printf or puts function
  * Return: (1) (Success)
  *
  */
 int main(void)
+{
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
+}
