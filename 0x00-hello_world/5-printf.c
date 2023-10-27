204f3f9eb127582a29047d401944e5b1ed9f3858
Adjocacher Abednego <abednegoadjocacher@gmail.com>
2023-10-26 18:48:00 -0700

    Usage of printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Printing a program with printf function
+ * Return: o
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
