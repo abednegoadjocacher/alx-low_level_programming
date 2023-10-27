1d64c13fc03b0c958a4868f9205fe246ecac7ee6
Adjocacher Abednego <abednegoadjocacher@gmail.com>
2023-10-26 18:39:49 -0700

    Printing with the puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
- * main is void
- * A program that prints a line with puts function
+ * main - A program that prints a line with puts function
  * Return: 0
  */
 int main(void)
