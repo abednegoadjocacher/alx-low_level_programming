a7b70cfc80f1b475ff5996f8613bac9239e35083
Adjocacher Abednego <abednegoadjocacher@gmail.com>
2023-10-28 06:28:36 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,21 +6,21 @@
  */
 int main(void)
 {
-        int a, b;
+	int a, b;
 
 	for (a = 0; a <= 98; a++)
 	{
 	for (b = a + 1; b <= 99; b++)
 	{
-		putchar((a / 10) + '0');
-		putchar((a % 10) + '0');
-		putchar(' ');
-		putchar((b / 10) + '0');
-		putchar((b % 10) + '0');
-		if (a == 98 && b == 99)
-		continue;
-		putchar(',');
-		putchar(' ');
+	putchar((a / 10) + '0');
+	putchar((a % 10) + '0');
+	putchar(' ');
+	putchar((b / 10) + '0');
+	putchar((b % 10) + '0');
+	if (a == 98 && b == 99)
+	continue;
+	putchar(',');
+	putchar(' ');
 	}
 	}
 	putchar('\n');
