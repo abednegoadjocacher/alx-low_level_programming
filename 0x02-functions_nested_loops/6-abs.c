e2f119d8a1e7629a1d395c569f0f36bfd26fb21a
Adjocacher Abednego <abednegoadjocacher@gmail.com>
2023-11-01 14:34:27 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * int _abs - Absolute integer
+ * @c: Computed number
+ * Return: Absolute value of number or zero
+ */
+int _abs(int c)
+{
+
+	if (c < 0)
+	{
+	int abs_val;
+	abs_val = c * -1;
+	return (abs_val);
+	}
+	return (c);
+}
