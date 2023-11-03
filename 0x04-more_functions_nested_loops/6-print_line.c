94a5183f50dd56abc25c8813f92fa1cecbaa4b8d
Adjocacher Abednego <abednegoadjocacher@gmail.com>
2023-11-03 07:52:04 -0700

    print_line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,11 +8,15 @@ void print_line(int n)
 {
 	int i;
 
+	if (n <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
 	for (i = 0; i < n; i++)
 	{
-	if (n > 0)
-	{
-	_putchar('_');
+	_putchar(95);
 	}
 	_putchar('\n');
 	}
