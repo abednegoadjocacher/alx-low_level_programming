de50c3689b2798bc6d25151de1c16047c866520f
Adjocacher Abednego <abednegoadjocacher@gmail.com>
2023-11-03 03:38:25 -0700

    print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - Numbers
+ * Return: nothing
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i >= 9; i++)
+	{
+	_putchar(i);
+	}
+	_putchar('\n');
+}
