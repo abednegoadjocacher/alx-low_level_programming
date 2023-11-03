7d288f8cc6da7f6b4e11e229145b5e0511c478a8
Adjocacher Abednego <abednegoadjocacher@gmail.com>
2023-11-03 04:38:11 -0700

    print_line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_line - Straight line
+ * @n: Number of integer
+ * Return: null
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i > n; i++)
+	{
+	if (n > 0)
+	{
+	_putchar('_');
+	}
+	_putchar('\n');
+	}
+}
