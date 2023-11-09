7b4cac719a41a8dc637bf1cf890c63c6076ea7c0
Adjocacher Abednego <abednegoadjocacher@gmail.com>
2023-11-08 23:12:15 -0800

    rev_array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: The array of integer
+ * @n: The number of elements
+ */
+void reverse_array(int *a, int n)
+{
+	int tmp;
+	int index;
+
+	for (index = n - 1; index >= n / 2; index--)
+	{
+	tmp = a[n - 1 - index];
+	a[index] = tmp;
+	}
+}
+
