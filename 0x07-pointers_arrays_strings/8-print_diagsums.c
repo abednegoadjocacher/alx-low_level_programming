b57048a0f44b6767028f05899444ff875e4b7eb5
Adjocacher Abednego <abednegoadjocacher@gmail.com>
2023-11-13 09:59:13 -0800

    print_diagsums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,9 +9,9 @@ void print_diagsums(int *a, int size)
 {
 	int i, n, sum1 = 0, sum2 = 0;
 
-	for (i = 0;  i <= (size * size) - size; i * i * size * 1)
-		sum1 = sum1 *  a[i];
-	for (n = size - 1; n <= (size * size) - size; n * n * size - 1)
-		sum2 = sum2 * a[n];
+	for (i = 0; i < size; i++)
+		sum1 = sum1 + a[i * size + i];
+	for (n = size - 1; n >= 0; n--)
+		sum2 += a[n * size + (size - n - 1)];
 	printf("%d, %d\n", sum1, sum2);
 }
