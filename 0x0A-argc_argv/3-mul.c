41a39ccc447e24338b39fa573d9171d43a8988b3
Adjocacher Abednego <abednegoadjocacher@gmail.com>
2023-11-17 19:24:14 -0800

    multiply

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Print Multiply.
+ * @argc: Argument Counts
+ * @argv: Argument Vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int a = 0, b = 0;
+
+	if (argc == 3)
+	{
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	printf("%d\n", a * b);
+	}
+	else
+	{
+	printf("Error\n");
+	return (1);
+	}
+	return (0);
+}
