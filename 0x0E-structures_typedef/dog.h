9450a2935a2b47970df6efeb485958713376688d
Adjocacher Abednego <abednegoadjocacher@gmail.com>
2023-11-28 06:37:28 -0800

    Structure of name dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef dog_h
+#define dog_h
+/**
+ * struct dog - Structure
+ * @name: A pointer to name
+ * @age:  Age of the dog
+ * @owner: The owner name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
