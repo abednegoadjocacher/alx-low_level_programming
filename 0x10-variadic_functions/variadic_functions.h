7b6c08a116fa8a140c97110e158523bd3c851b08
Adjocacher Abednego <abednegoadjocacher@gmail.com>
2023-11-30 10:52:17 -0800

    summing all arguments

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,8 @@
 #ifndef variadic_functions_h
 #define variadic_functions_h
+#include <stdio.h>
+#include <stdarg.h>
+#include <stddef.h>
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
