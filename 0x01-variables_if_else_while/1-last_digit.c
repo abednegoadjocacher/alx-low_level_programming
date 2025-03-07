685b4f93b40f3adffa15289af2a535af5c303c07
abednegoadjocacher <abednegoadjocacher@gmail.com>
2025-03-07 05:56:55 -0800

    Print last digit with modulo

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ int main(void)
 	printf("Last digit of %d is %d and is greater than 5\n", n, _digit);
 	else if (_digit == 0)
 	printf("Last digit of %d is %d and is zero 0\n", n, _digit);
-	else
+	else if (_digit > 6 && _digit != 0)
 	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, _digit);
 	return (0);
 }
