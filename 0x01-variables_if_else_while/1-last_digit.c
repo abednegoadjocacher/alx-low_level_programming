320d37e584d73c65de17e144b9487e0d883b261c
abednegoadjocacher <abednegoadjocacher@gmail.com>
2025-03-07 05:45:15 -0800

    Print last digit with modulo

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,10 +15,10 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	_digit = n % 10;
 	if (_digit > 5)
-	printf("Last digit of %d is and %d is greater than 5\n", n, _digit);
+	printf("Last digit of %d is %d and is greater than 5\n", n, _digit);
 	else if (_digit == 0)
 	printf("Last digit of %d is %d and is zero 0\n", n, _digit);
 	else
-	printf("Last digit of %d is and %d is less than 6 and not 0\n", n, _digit);
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, _digit);
 	return (0);
 }
