93a2d234652a5867f56635201662a94d459f013b
abednegoadjocacher <abednegoadjocacher@gmail.com>
2025-03-12 15:26:33 -0700

    Print the time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,37 @@
+#include"main.h"
+/**
+ * jack_bauer - I have a long day
+ *
+ * Return: nothing
+ */
+void jack_bauer(void)
+{
+	int first_hr, second_hr, mins, sec;
+
+	first_hr = second_hr = mins = sec = 0;
+	while (first_hr <= 2)
+	{
+		second_hr = 0;
+		while (second_hr <= 3)
+		{
+			mins = 0;
+			while (mins <= 5)
+			{
+				sec = 0;
+				while (sec <= 9)
+				{
+					_putchar(first_hr + '0');
+					_putchar(second_hr + '0');
+					_putchar(':');
+					_putchar(mins + '0');
+					_putchar(sec + '0');
+					sec++;
+					_putchar('\n');
+				}
+			mins++;
+		}
+		second_hr++;
+	}
+	first_hr++;
+}
+}
