8ca77fc0d8cbb91dabbdee43d6f7335a4790ef93
abednegoadjocacher <abednegoadjocacher@gmail.com>
2025-03-13 12:20:29 -0700

    Print time table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+#include"main.h"
+/**
+ * times_table - Print 9 times table
+ * Return: void
+ */
+void times_table(void)
+{
+	int number, times, product;
+
+	number = times = product = 0;
+	while (number <= 9)
+	{
+		times = 0;
+		while (times <= 9)
+		{
+			product = number * times;
+			if (product <= 9)
+			{
+				_putchar(product + '0');
+				times++;
+				if (times <= 9)
+				{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				}
+			}
+			else if (product > 9)
+			{
+			_putchar(product / 10 + '0');
+			_putchar(product % 10 + '0');
+			times++;
+			if (times <= 9)
+			{
+			_putchar(',');
+			_putchar(' ');
+			}
+			}
+		}
+		_putchar('\n');
+		number++;
+	}
+}
