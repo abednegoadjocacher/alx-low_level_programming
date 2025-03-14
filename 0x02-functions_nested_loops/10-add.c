28b9163b6701c0974bd337a39022e1aa0f035457
abednegoadjocacher <abednegoadjocacher@gmail.com>
2025-03-14 10:28:17 -0700

    Add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+ * add - Add two number
+ * @n1: Number one
+ * @n2: Number two
+ * Return: Sum
+ */
+int add(int n1, int n2)
+{
+	int sum = 0;
+
+	sum = n1 + n2;
+	return (sum);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,7 +21,7 @@ void times_table(void)
 				if (times <= 9)
 				{
 				_putchar(',');
-				_putchar(' ');
+				_putchar(' '); 
 				_putchar(' ');
 				}
 			}
