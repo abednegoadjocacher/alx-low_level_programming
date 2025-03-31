35363ac208d993a3e596aa987be44b7120eb0b6b
abednegoadjocacher <abednegoadjocacher@gmail.com>
2025-03-31 14:01:14 -0700

    Uppercase

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isdigit - Check digit
+ * @c: Input digit
+ * Return: 1 for true or 0 for false
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+	return (1);
+	}
+	return (0);
+}
