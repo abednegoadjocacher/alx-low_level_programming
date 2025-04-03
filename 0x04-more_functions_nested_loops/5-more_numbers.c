cc958baf2dc0e0a998c5a313fafe2701fadee733
abednegoadjocacher <abednegoadjocacher@gmail.com>
2025-04-02 17:05:02 -0700

    Numbers can speak

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,13 +13,9 @@ void more_numbers(void)
 
 		while (c <= 14)
 		{
-			if (c <= 9)
-				_putchar(c + '0');
-			else
-			{
-				_putchar(c / 10 + '0');
-				_putchar(c % 10 + '0');
-			}
+			if (c > 9)
+				_putchar('1');
+			_putchar(c % 10 + '0');
 			c++;
 		}
 		_putchar('\n');
