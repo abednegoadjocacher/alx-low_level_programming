ad32720c23eb2a93b73094fc8e050a2ecd08504d
abednegoadjocacher <abednegoadjocacher@gmail.com>
2025-04-05 15:48:10 -0700

    The Fizz Buzz test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+/**
+ * main - Entrying point
+ * Print number from 1 to 100
+ * Check if number is divisible by 3, 5 or both
+ * If number is divisible by 3 print Fizz
+ * Else if number divisible by 5 print Buzz
+ * Else if both divides print FizzBuzz
+ * Return: 0;
+ */
+int main(void)
+{
+	int number = 1;
+
+	while (number <= 100)
+	{
+		if (number % 3 == 0 && number % 5 == 0)
+			printf("FizzBuzz ");
+		else if (number % 5 == 0)
+			printf("Buzz ");
+		else if (number % 3 == 0)
+			printf("Fizz ");
+		else
+			printf("%d ", number);
+		number++;
+	}
+	return (0);
+}
