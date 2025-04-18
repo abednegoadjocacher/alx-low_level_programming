b6ea3c89c683dd6f07bb8c70828e2f846b9793e2
abednegoadjocacher <abednegoadjocacher@gmail.com>
2025-04-18 06:10:19 -0700

    A function that reverse a string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,16 +8,16 @@ void print_rev(char *s)
 {
 	int length_string = 0;
 
-        while(s[length_string] != '\0')
-        {
-                length_string++; 
-        }
+	while (s[length_string] != '\0')
+	{
+		length_string++;
+	}
 	length_string -= 1;
 	while (length_string >= 0)
 	{
 		_putchar(s[length_string]);
 		length_string--;
 	}
-        _putchar('\n');
+	_putchar('\n');
 }
 
