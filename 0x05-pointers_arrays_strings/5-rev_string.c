4fc3a56dd63d2282309a19f03eb22ca15ace0af3
abednegoadjocacher <abednegoadjocacher@gmail.com>
2025-04-18 08:04:28 -0700

    Reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include"main.h"
+/**
+ * rev_string - Prints a string in reverse with a newline
+ * @s: The pointer to string
+ * Return: void
+ */
+void rev_string(char *s)
+{
+        int length = 0;
+	int i = 0;
+	char temp;
+
+        while (s[length] != '\0')
+        {
+                length++;
+        }
+        length -= 1;
+        while (i < length)
+        {
+                temp = s[i];
+		s[i] = s[length];
+		s[length] = temp;
+		i++;
+                length--;
+        }
+}
+
