7ee3725ba533b27c7fdaee40a602610cb726d9a9
abednegoadjocacher <abednegoadjocacher@gmail.com>
2025-04-18 08:09:42 -0700

    Reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,22 +6,22 @@
  */
 void rev_string(char *s)
 {
-        int length = 0;
+	int length = 0;
 	int i = 0;
 	char temp;
 
-        while (s[length] != '\0')
-        {
-                length++;
-        }
-        length -= 1;
-        while (i < length)
-        {
-                temp = s[i];
+	while (s[length] != '\0')
+	{
+	length++;
+	}
+	length -= 1;
+	while (i < length)
+	{
+		temp = s[i];
 		s[i] = s[length];
 		s[length] = temp;
-		i++;
-                length--;
-        }
+		i++
+		length--;
+	}
 }
 
