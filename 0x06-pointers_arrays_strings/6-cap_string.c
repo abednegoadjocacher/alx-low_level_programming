796e5cc0d2a7151e2d3de576c873c401438bc1b1
abednegoadjocacher <abednegoadjocacher@gmail.com>
2025-08-14 15:52:20 -0700

    Capitalize the beginning of every world

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,15 +11,16 @@ char *cap_string(char *s)
 {
 	int i = 0;
 	int flag = 0;
+
 	while (s[i] != '\0')
 	{
 		if (s[i] == ' ' || s[i] == '\t' || s[i] == '.' || s[i] == ',' || s[i] == '"' || s[i] == '?' || s[i] == '!' || s[i] == ';' || s[i] == '(' || s[i] == ')' || s[i] == '{' || s[i] == '}' || s[i] == '\n')
 		{
 			flag = 1;
-		}        
+		}
 		else if  (flag && s[i] >= 97 && s[i] <= 122)
 		{
-			flag = 0; 
+			flag = 0;
 			s[i] -= 32;
 		}
 		else
