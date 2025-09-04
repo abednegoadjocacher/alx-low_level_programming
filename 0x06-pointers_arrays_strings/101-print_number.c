a39726520f7ddd3ee955d9ceae59376b52eefbe1
abednegoadjocacher <abednegoadjocacher@gmail.com>
2025-09-04 11:29:14 -0700

    Print numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,53 +1,32 @@
 #include "main.h"
-
 /**
- * print_number:A function that print numbers
- * @: A number
- * Return void
+ * print_number - Prints an integer using _putchar
+ * @n: The integer to print
+ *
+ * Return: void
  */
-
 void print_number(int n)
 {
-	int temp, r;
-	int v, _n;
-	int reminder, m;
+	int divisor = 1;
+	int temp = n;
+	int digit;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n *= -1;
+		n = -n;
+		temp = n;
 	}
-	if (n < 10)
+	while (temp >= 10)
 	{
-		_putchar(n + '0');
+		temp /= 10;
+		divisor *= 10;
 	}
-	else if(n >= 10 && n <= 99)
+	while (divisor >= 1)
 	{
-		v = n / 10;
-		reminder = n % 10;
-		_putchar(v + '0');
-	       _putchar(reminder + '0');
-	}
-	else if (n >= 100 && n <= 999)
-	{
-		v = n / 100;
-		reminder = n % 100;
-		r = reminder / 10;
-		temp = reminder % 10;
-		_putchar(v + '0');
-		_putchar(r + '0');
-	       _putchar(temp + '0');
-	}
-	else if (n >= 1000 && n <= 9999)
-	{
-		v = n / 1000;
-		reminder = n % 1000;
-		r  = reminder / 100;
-		temp = reminder % 100;
-		_n = temp / 10;
-		m = temp % 10;
-		_putchar(v + '0');
-		_putchar(r + '0');
-		_putchar(_n + '0');
-		_putchar(m + '0');
+		digit = n / divisor;
+		_putchar(digit + '0');
+		n %= divisor;
+		divisor /= 10;
 	}
 }
