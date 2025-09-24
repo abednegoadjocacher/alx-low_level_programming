ad7d7eb21e2b9c5b0b99fb49de92b6495edf0dbb
abednegoadjocacher <abednegoadjocacher@gmail.com>
2025-09-24 16:26:06 -0700

    A function that gets the length of a prefix substring.

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,28 +7,28 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-    int lenght = 0;
-    int j;
-    int found;
+	int lenght = 0;
+	int j;
+	int found;
 
-    while (s[lenght] != '\0')
-    {       
-        j = 0;
-        found = 1;
-        while (accept[j] != '\0')
-        {
-            if (s[lenght] == accept[j])
-            {
-                found = 0;
-                break;
-            }
-            j++;
-        }
-        if (found == 1)
-        {
-            break;
-        }
-        lenght++; 
-    }
-    return (lenght);
+	while (s[lenght] != '\0')
+	{
+		j = 0;
+		found = 1;
+		while (accept[j] != '\0')
+		{
+			if (s[lenght] == accept[j])
+			{
+				found = 0;
+				break;
+			}
+		j++;
+		}
+		if (found == 1)
+		{
+			break;
+		}
+			lenght++;
+	}
+	return (lenght);
 }
